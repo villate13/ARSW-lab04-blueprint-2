0ca6dd64e3f709a9eac0e4ca8eca8cb60b2171e8
Juan Villate <juan.villate-i@mail.escuelaing.edu.co>
2020-02-18 23:37:28 +0530

    PART II - Finalizado

--- src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
+++ src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
@@ -5,25 +5,36 @@
  */
 package edu.eci.arsw.blueprints.controllers;
 
+
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import edu.eci.arsw.blueprints.model.Blueprint;
+import edu.eci.arsw.blueprints.model.Point;
+
 import edu.eci.arsw.blueprints.persistence.BlueprintNotFoundException;
+import edu.eci.arsw.blueprints.persistence.BlueprintPersistenceException;
 import edu.eci.arsw.blueprints.services.BlueprintsServices;
+import java.lang.reflect.Type;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  *
@@ -114,5 +125,59 @@ public class BlueprintAPIController {
         }
     }   
     
+    /**
+     * 
+     * @param blueprint
+     * @return status http servidor
+     * @throws BlueprintPersistenceException 
+     */
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> addNewBlueprint(@RequestBody String bp) throws BlueprintPersistenceException {
+        //Formato json {"bp5":{"author":"Isaza","points":[{"x":180,"y":181},{"x":190,"y":190}],"name":"bp5"}}
+        try {
+            //System.out.println("data"+bp);
+            Type jsonToList = new TypeToken<Map<String, Blueprint>>() {}.getType();
+            
+            Map<String, Blueprint> data = new Gson().fromJson(bp, jsonToList);
+
+            Object[] keys = data.keySet().toArray();
+            
+            //System.out.println("llave"+keys[0].toString());
+
+            bpServices.addNewBlueprint(data.get(keys[0]));
+
+            return new ResponseEntity<>(HttpStatus.CREATED);
+
+        } catch (BlueprintPersistenceException ex) {
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("ERROR. >> No se ha podido añadir el blueprint", HttpStatus.FORBIDDEN);
+        }
+    }
+    
+    @RequestMapping(method = RequestMethod.PUT, path = "{author}/{name}")
+    public ResponseEntity<?> updateABlueprint(@PathVariable("author") String author,
+            @PathVariable("name") String name, @RequestBody String points) throws BlueprintNotFoundException {
+        //Formato json {"1":[{"x":9,"y":9},{"x":10,"y":10}]}
+        try {
+            //System.out.println("puntos"+points);
+            Blueprint selectBP = bpServices.getBlueprint(author, name);
+
+            Type jsonToList = new TypeToken<Map<String, Point[]>>() {
+            }.getType();
+            Map<String, Point[]> data = new Gson().fromJson(points, jsonToList);
+
+            Object[] keys = data.keySet().toArray();
+            
+            //System.out.println("llave"+keys[0].toString());
+
+            selectBP.setPoints(Arrays.asList(data.get(keys[0])));
+
+            return new ResponseEntity<>(HttpStatus.OK);
+
+        } catch (BlueprintNotFoundException ex) {
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("ERROR, >> No se ha podido actualizar el blueprint", HttpStatus.FORBIDDEN);
+        }
+    }
 }
 
